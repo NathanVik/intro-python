fe7b347d4669287cf06491342cb203edc4a01f70
NathanV <nathan.t.vik@gmail.com>
2021-08-12 13:54:26 -0400

    Added message repeat function

--- calc.py
+++ calc.py
@@ -13,7 +13,15 @@ from display import print_menu
 
 # functions
 
+def message():
+    try:
+        msg = input("Type your Message:") + "\n"
+        num = int(input("Number of times to repeat:"))
+        print(msg*num)
+    except:
+            print("Invalid input, try again")
 
+            
 # plain instructions
 opt = ""
 while(opt != "x"):
@@ -24,27 +32,29 @@ while(opt != "x"):
     opt = input("Choose an option:")
     if(opt == "x"):
         break
+    elif(opt == "5"):
+        message()
+    else:
+        try:
+            num1 = float(input("First number:"))
+            num2 = float(input("Second number:"))
 
-    try:
-        num1 = float(input("First number:"))
-        num2 = float(input("Second number:"))
+        except:
+            error = True
+            print("Invalid input, try again")
 
-    except:
-        error = True
-        print("Invalid input, try again")
-
-    if(not error):    
-        if(opt == "1"):
-            print(f"The Result is: {num1 + num2}")
-        elif(opt == "2"):
-            print(f"The Result is: {num1 - num2}")
-        elif(opt == "3"):
-            print(f"The Result is: {num1 * num2}")
-        elif(opt == "4"):
-            if(num2 == 0):
-                print("Can't divide by 0!")
-            else:
-                print(f"The Result is: {num1 / num2}")
+        if(not error):    
+            if(opt == "1"):
+                print(f"The Result is: {num1 + num2}")
+            elif(opt == "2"):
+                print(f"The Result is: {num1 - num2}")
+            elif(opt == "3"):
+                print(f"The Result is: {num1 * num2}")
+            elif(opt == "4"):
+                if(num2 == 0):
+                    print("Can't divide by 0!")
+                else:
+                    print(f"The Result is: {num1 / num2}")
     
     input("Press Enter to Continue")
 
--- display.py
+++ display.py
@@ -3,12 +3,13 @@
 def print_menu():
     print("")
     print("*******************")
-    print("  Simple PyCalc  ")
+    print("   Simple PyCalc   ")
     print("*******************")
 
     print("[1] Sum")
     print("[2] Subtract")
     print("[3] Multiply")
     print("[4] Divide")
-
+    print("[5] Display a Message")
+    
     print("[x] Close")
